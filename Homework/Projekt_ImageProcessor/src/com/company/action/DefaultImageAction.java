22656765ce2348ab561dfd6c9c85d341b93ea42c
mariajanzen <maria_yantsen@yahoo.com>
2020-11-23 10:21:41 +0100

    All the rest to the Project

---
+++ Homework/Projekt_ImageProcessor/src/com/company/action/DefaultImageAction.java
@@ -0,0 +1,17 @@
+package com.company.action;
+
+import java.awt.image.BufferedImage;
+
+public class DefaultImageAction implements ImageAction {
+
+    @Override
+    public String getName() {
+        return "DEFAULT";
+    }
+
+    @Override
+    public BufferedImage doAction(BufferedImage image) throws Exception {
+        System.out.println("default action");
+        throw new Exception("Action not supported");
+    }
+}
---
+++ Homework/Projekt_ImageProcessor/src/com/company/action/PreviewImageAction.java
@@ -0,0 +1,16 @@
+package com.company.action;
+
+import java.awt.image.BufferedImage;
+
+public class PreviewImageAction implements ImageAction {
+    @Override
+    public String getName() {
+        return "PREVIEW";
+    }
+
+    @Override
+    public BufferedImage doAction(BufferedImage image) throws Exception {
+        System.out.println("Generating a preview");
+        return null;
+    }
+}
