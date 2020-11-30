11db439ad90ebda576195bf904a49c67af35d99b
mariajanzen <maria_yantsen@yahoo.com>
2020-11-30 10:34:35 +0100

    05112020

---
+++ Java/Projekt_IT_57_CW_05112020/src/com/company/exceptions/InvalidCustomerAgeException.java
@@ -0,0 +1,8 @@
+package com.company.exceptions;
+
+public class InvalidCustomerAgeException extends Exception {
+
+    public InvalidCustomerAgeException(String message) {
+        super(message);
+    }
+}
